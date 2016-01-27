3b167e4e3acc83a1c5cf9a688c42aded02fa50fa
Martin Hartl <martin@mhaddl.me>
2016-01-27 10:37:40 +0100

    Add custom Modulemap file for project

---
+++ MHCustomTabBarController/MHCustomTabBarControllerUmbrellaHeader.h
@@ -0,0 +1,21 @@
+//
+//  MHCustomTabBarControllerUmbrellaHeader.h
+//  MHCustomTabBarControllerUmbrellaHeader
+//
+//  Created by Martin Hartl on 27/01/16.
+//  Copyright © 2016 Martin Hartl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Test.
+FOUNDATION_EXPORT double TestVersionNumber;
+
+//! Project version string for Test.
+FOUNDATION_EXPORT const unsigned char TestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Test/PublicHeader.h>
+
+
+#import <MHCustomTabbarController/MHCustomTabBarController.h>
+#import <MHCustomTabbarController/MHTabBarSegue.h>
